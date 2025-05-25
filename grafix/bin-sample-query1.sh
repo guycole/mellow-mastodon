6ceb38bb6c9be003de0d59ae048a22217b0b3687
Guy Cole <guycole@gmail.com>
2025-05-25 01:55:53 -0700

    updated plot

--- grafix/bin-sample-query1.sh
+++ grafix/bin-sample-query1.sh
@@ -10,11 +10,11 @@ export PGHOST=localhost
 export PGPASSWORD=batabat
 export PGUSER=mastodon_client
 #
-psql $PGDATABASE -c "select freq_hz, signal_dbm from mastodon_v1.bin_sample where row_head_id = 1 order by bin_ndx;" > bin-sample1.txt
+psql $PGDATABASE -c "select freq_hz, signal_dbm from mastodon_v1.bin_sample where row_head_id < 5 order by freq_hz;" > bin-sample1.txt
 tr -d '|' < bin-sample1.txt > bin-sample1.dat
 gnuplot -p bin-sample1.gp
 #
-psql $PGDATABASE -c "select freq_hz, rolling_mean, signal_dbm from mastodon_v1.bin_sample where row_head_id = 1 order by bin_ndx;" > bin-sample2.txt
+psql $PGDATABASE -c "select freq_hz, rolling_mean, signal_dbm from mastodon_v1.bin_sample where row_head_id < 5 order by freq_hz;" > bin-sample2.txt
 tr -d '|' < bin-sample2.txt > bin-sample2.dat
 gnuplot -p bin-sample2.gp
 #
