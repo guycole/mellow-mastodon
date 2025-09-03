960cf46d215b8ed7928d80843660449c214c25f5
Guy Cole <guycole@gmail.com>
2025-09-03 19:52:57 +0000

    directory cleanup

--- bin/csv2json.sh
+++ bin/csv2json.sh
@@ -10,7 +10,6 @@ PATH=/bin:/usr/bin:/etc:/usr/local/bin; export PATH
 HOME_DIR="/home/gsc/Documents/github"
 #HOME_DIR="/Users/gsc/Documents/github"
 #
-COOKED_DIR="cooked"
 PROCESSED_DIR="processed"
 WORK_DIR="/var/mellow/mastodon"
 #
@@ -20,9 +19,6 @@ source venv/bin/activate
 time python3 ./csv2json.py
 #
 cd $WORK_DIR
-#
-rm -rf ${COOKED_DIR}
-mkdir ${COOKED_DIR}
 #
 rm -rf ${PROCESSED_DIR}
 mkdir ${PROCESSED_DIR}
